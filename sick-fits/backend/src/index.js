981fbfd2d25aeeca7cc7160e004533b0805a6400
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-02-16 21:33:06 +0100

    Fix server - add cors

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,14 +1,19 @@
-require('dotenv').config({path: '.env'});
-const createServer = require('./createServer');
-const db = require('./db');
+require("dotenv").config({ path: ".env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
 const server = createServer();
 
 // TODO: Use express middleware to handle JWT cookies & populate current user
 
-server.start({
-  credentials: true,
-  origin: process.env.FRONTEND_URL,
-}, deets => {
-  console.log(`Server is now running on port http://localhost:${deets.port}`);
-})
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
