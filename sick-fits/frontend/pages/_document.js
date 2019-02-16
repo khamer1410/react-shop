bda83e88245e1d2d7a94085b355154b020bfc78e
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-02-16 20:50:55 +0100

    Add SSR & AppolloProvider

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
+// Next patent to prevent FOUC - grab styles for page before first render
 // TODO: try to switch it into updated version showed in documentation with updated dependencies
 export default class MyDocument extends Document {
   static getInitialProps ({ renderPage }) {
