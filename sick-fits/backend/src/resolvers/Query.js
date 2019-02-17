ea51f286a22f17a9461aed1f6f6b18855c8b2466
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-02-17 21:43:22 +0100

    BE - add updateItem querry

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,11 +2,13 @@ const {forwardTo} = require('prisma-binding')
 
 const Query = {
   //you can use shorthand if you only pass the data without auth or logic
-  items: forwardTo('db')
+  items: forwardTo('db'),
 
   // async items(parent, args, ctx, info) {
   //   return await ctx.db.query.items()
   // }
+
+  item: forwardTo('db'),
 };
 
 module.exports = Query;
