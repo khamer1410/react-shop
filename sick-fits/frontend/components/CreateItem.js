245c4e0e50c138bdce1f5bccd7fe86b6a0e83afc
Krzysztof Hamerszmit <kkhamer@gmail.com>
2019-02-17 21:16:13 +0100

    CreateItem add image upload at cloudinary

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import gql from 'graphql-tag';
-import Router from 'next/router';
-import Form from './styles/Form';
-import formatMoney from '../lib/formatMoney';
-import Error from './ErrorMessage';
+import React, { Component } from "react";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import styled from 'styled-components';
+import Router from "next/router";
+import Form from "./styles/Form";
+import formatMoney from "../lib/formatMoney";
+import Error from "./ErrorMessage";
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -28,18 +29,43 @@ const CREATE_ITEM_MUTATION = gql`
 
 class CreateItem extends Component {
   state = {
-    title: 'Cool Shoes',
-    description: 'I love those shoes',
-    image: 'dog.jpg',
-    largeImage: 'large-dog.jpg',
-    price: 1000,
+    title: "Cool Shoes",
+    description: "I love those shoes",
+    image: "",
+    largeImage: "",
+    price: 1000
   };
+
   handleChange = e => {
     const { name, type, value } = e.target;
-    const val = type === 'number' ? parseFloat(value) : value;
+    const val = type === "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+
+  uploadFile = async event => {
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const response = await fetch(
+      "https://api.cloudinary.com/v1_1/hamercloud/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    )
+
+    const file = await response.json()
+
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    })
+  }
+
   render() {
+    const { title, description, image, price } = this.state
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
@@ -48,17 +74,30 @@ class CreateItem extends Component {
               // Stop the form from submitting
               e.preventDefault();
               // call the mutation
-              const res = await createItem();
+              const response = await createItem();
               // change them to the single item page
-              console.log(res);
-              // Router.push({
-              //   pathname: '/item',
-              //   query: { id: res.data.createItem.id },
-              // });
+              console.log(response);
+              Router.push({
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
+              });
             }}
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {image && <ImagePreview src={image} alt='upload preview'/>}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
@@ -67,7 +106,7 @@ class CreateItem extends Component {
                   name="title"
                   placeholder="Title"
                   required
-                  value={this.state.title}
+                  value={title}
                   onChange={this.handleChange}
                 />
               </label>
@@ -80,7 +119,7 @@ class CreateItem extends Component {
                   name="price"
                   placeholder="Price"
                   required
-                  value={this.state.price}
+                  value={price}
                   onChange={this.handleChange}
                 />
               </label>
@@ -92,7 +131,7 @@ class CreateItem extends Component {
                   name="description"
                   placeholder="Enter A Description"
                   required
-                  value={this.state.description}
+                  value={description}
                   onChange={this.handleChange}
                 />
               </label>
@@ -107,3 +146,7 @@ class CreateItem extends Component {
 
 export default CreateItem;
 export { CREATE_ITEM_MUTATION };
+
+const ImagePreview = styled.img`
+  max-width: 100%;
+`
